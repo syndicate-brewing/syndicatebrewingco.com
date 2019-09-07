ecde56b0f30993d06ec6ed8756e761fa3ea24670
Aaron Ricketson <aaron.ricketson@gmail.com>
2019-09-07 14:09:14 -0600

    Add favicon

--- config.rb
+++ config.rb
@@ -2,6 +2,6 @@ activate :sprockets
 sprockets.append_path "node_modules"
 
 configure :build do
-  activate :asset_hash
+  activate :asset_hash, ignore: /syndicate-icon/
   activate :minify_css
 end

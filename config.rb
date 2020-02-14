2147c4dcbe014bc5825def12683158dca5cc1b90
Aaron Ricketson <aaron.ricketson@gmail.com>
2020-02-14 08:23:04 -0700

    Update asset build

--- config.rb
+++ config.rb
@@ -1,5 +1,12 @@
 WEBPACK_CLI = "./node_modules/webpack-cli/bin/cli.js".freeze
 
+configure :build do
+  activate :asset_hash
+  activate :minify_css
+  activate :minify_javascript
+end
+
+activate :relative_assets
 activate :directory_indexes
 activate :livereload
 activate :external_pipeline,
@@ -17,3 +24,25 @@ helpers do
     end
   end
 end
+
+class ZapContentLength < Struct.new(:app)
+  def call(env)
+    s, h, b = app.call(env)
+    # The URL rewriters in Middleman do not update Content-Length correctly,
+    # which makes Rack-Lint flag the responses as having a wrong Content-Length.
+    # For building assets this has zero importance because the Content-Length
+    # header will be discarded - it is the server that recomputes it. But
+    # it does prevent the site from building correctly.
+    #
+    # The fastest way out of this is to let Rack recompute the Content-Length
+    # forcibly, for every response, at retrieval time.
+    #
+    # See https://github.com/middleman/middleman/issues/2309
+    # and https://github.com/rack/rack/issues/1472
+    h.delete('Content-Length')
+    [s, h, b]
+  end
+end
+
+app.use ::Rack::ContentLength
+app.use ZapContentLength
